24d64134cf663381c60b8db2ecefaea2a7248bee
Arkam <Arkamshakil@gmail.com>
2021-09-07 09:31:11 +0530

    My Second JavaScript Assignment. The folder is named as SentEmailAssignment

---
+++ SendEmailAssignment/JS/SendEmailAssignment.js
@@ -0,0 +1,38 @@
+function sentMail(frontPage, backPage, toName, sub, message, heading, details, messageDetails, error)
+{
+	let to = document.getElementById(toName).value;
+	let subject = document.getElementById(sub).value;
+	let msg = document.getElementById(message).value;
+	
+	if ((to.length > 5)&&(subject.length > 3)&&(msg.length > 5))
+	{
+		document.getElementById(frontPage).setAttribute("style", "display: none;");
+		document.getElementById(backPage).setAttribute("style", "display: initial;");
+		
+		document.getElementById(heading).innerHTML = subject;
+		document.getElementById(details).innerHTML = "Sent to: " + to + "<br>Sent at: " + Date();
+		document.getElementById(messageDetails).innerHTML = "Message:<br>" + msg;
+		document.getElementById(heading).setAttribute("tabindex", "-1");
+		document.getElementById(heading).focus();
+	}
+	else {
+		document.getElementById(error).setAttribute("tabindex", "-1");
+		document.getElementById(error).removeAttribute("role", "alert");
+		document.getElementById(error).setAttribute("role", "alert");
+		document.getElementById(error).innerHTML = "Please enter the details for all the required fields";
+		document.getElementById(error).focus();
+	}
+}
+
+function sendAnotherMail(frontPage, backPage)
+{
+	document.getElementById(frontPage).setAttribute("style", "display: initial;");
+	document.getElementById(backPage).setAttribute("style", "display: none;");
+}
+
+function appendAttachments()
+{
+	let attachment = '<span><input type="file"><br><button onclick="appendAttachments()">Attach More Files</button><br></span>';
+	document.getElementById("file").innerHTML += attachment;
+	
+}
